7255475ef29d931644f40e59b31622b8a9f46b1a
joster1697 <jorgemendoza1697@gmail.com>
2022-05-05 02:14:23 +0000

    generador finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cartaSimbolos = document.querySelectorAll(".simbolo");
+  let cartaNumero = document.getElementById("numero");
+  console.log(cartaNumero);
+
+  let simbolos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let simboloRandom = numeroRandom(0, 3);
+  if (simboloRandom == 0 || simboloRandom == 1) {
+    cartaSimbolos[0].style.color = "red";
+    cartaSimbolos[1].style.color = "red";
+  } else {
+    cartaSimbolos[0].style.color = "black";
+    cartaSimbolos[1].style.color = "black";
+  }
+  cartaSimbolos[0].innerHTML = simbolos[simboloRandom];
+  cartaSimbolos[1].innerHTML = simbolos[simboloRandom];
+  cartaSimbolos[1].style.transform = "rotate(180deg)";
+
+  cartaNumero.innerHTML = numeros[numeroRandom(0, 12)];
 };
+
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
